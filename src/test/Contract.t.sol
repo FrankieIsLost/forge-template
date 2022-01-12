cd2501703cf54a8d1b94fc7f123b3530182e2817
Frankie <frankieislost@protonmail.com>
2022-01-12 14:08:29 -0800

    solhint

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -9,17 +9,20 @@ import {Hevm} from "./utils/Hevm.sol";
 contract ContractTest is DSTest {
     Hevm internal immutable hevm = Hevm(HEVM_ADDRESS);
 
-    Utilities internal utils = new Utilities();
-    address payable[] internal users = utils.createUsers(5);
+    Utilities internal utils;
+    address payable[] internal users;
 
-    function setUp() public {}
+    function setUp() public {
+        utils = new Utilities();
+        users = utils.createUsers(5);
+    }
 
     function testExample() public {
         address payable alice = users[0];
         console.log("alice's address", alice);
         address payable bob = users[1];
         hevm.prank(alice);
-        (bool sent, bytes memory data) = bob.call{value: 10 ether}("");
+        (bool sent, ) = bob.call{value: 10 ether}("");
         assertTrue(sent);
         assertGt(bob.balance, alice.balance);
     }
--- src/test/utils/Console.sol
+++ src/test/utils/Console.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0;
 
+/* solhint-disable */
 library console {
     address constant CONSOLE_ADDRESS =
         address(0x000000000000000000636F6e736F6c652e6c6f67);
