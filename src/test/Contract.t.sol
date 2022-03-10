20d2be08fcb41ad3b2af9939dc9e2045747a02a1
Andreas Bigger <abigger87@gmail.com>
2022-03-10 13:21:12 -0800

    :test_tube: remove tests

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -30,39 +30,4 @@ contract ContractTest is DSTest {
         assertTrue(sent);
         assertGt(bob.balance, alice.balance);
     }
-
-    function testLogArray() public {
-        address[] memory addresses = new address[](2);
-        addresses[0] = users[0];
-        addresses[1] = users[1];
-        console.log(addresses);
-    }
-
-    function testLogStrings() public {
-        string[] memory strings = new string[](2);
-        strings[0] = "string1";
-        strings[1] = "string2";
-        console.log(strings);
-    }
-
-    function testLogBools() public {
-        bool[] memory bools = new bool[](2);
-        bools[0] = true;
-        bools[1] = false;
-        console.log(bools);
-    }
-
-    function testLogBytes() public {
-        bytes32[] memory bytess = new bytes32[](2);
-        bytess[0] = bytes32(0);
-        bytess[1] = bytes32(0);
-        console.log(bytess);
-    }
-
-    function testLogUint() public {
-        uint256[] memory uints = new uint256[](2);
-        uints[0] = 1337;
-        uints[1] = 1338;
-        console.log(uints);
-    }
 }
